a1b472daf1151a30d978fdc163e051fb58487444
jonnyroux <90427373+jonnyroux@users.noreply.github.com>
2022-02-24 23:15:41 -0500

    modif header+login

--- templates/header.php
+++ templates/header.php
@@ -8,7 +8,8 @@
             <li id="service"><a href="/services" >Services</a></li>
             <li id="a_propos"><a href="/apropos" >À propos</a></li>
             <li id="contact"><a href="/contact"  >Nous contacter</a></li>
-            <li id="langue"><a href="/materiaux" >Matériaux à donner</a></li>
+            <li id="mat"><a href="/materiaux" >Matériaux à donner</a></li>
+            <li id="connexion" ><a href="/login" class="fa-solid fa-user-astronaut"></a></li>
                 
         </ul>
     </nav>    
